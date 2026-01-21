e3b429bb9697c21bfb6c8656c78c6af100787c59
Jean Oliveira <jcwilbertt@gmail.com>
2026-01-21 14:01:23 +0000

    Create ActivityItem.tsx

---
+++ src/components/ActivityItem.tsx
@@ -0,0 +1,8 @@
+import { CircleCheck, Circle, Loader2 } from "lucide-react";
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+
+
+export function ActivityItem() {
+
+}
