60c47fb1f3bee02a0669adecd20ae36477bf5958
Jean Oliveira <jcwilbertt@gmail.com>
2026-01-26 23:24:31 +0000

    .

--- src/components/CreateActivityDialog.tsx
+++ src/components/CreateActivityDialog.tsx
@@ -89,7 +89,7 @@ export function CreateActivityDialog({ tripId }: CreateActivityDialogProps) {
                         </div>
                     </div>
 
-                    <div className="grid grid-cols-2 gap-3">
+                    <div>
                         <DatePicker
                             date={date}
                             setDate={setDate}
